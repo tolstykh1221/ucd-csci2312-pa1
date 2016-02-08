dca5907fa327010358599e5e527f99d1cfc671f2
tolstykh1221 <slava.tolstykh@hotmail.com>
2016-02-07 21:36:28 -0700

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,4 +1,58 @@
-//
-// Created by Slava on 2/7/2016.
-//
+#include <cmath>
+#include "Point.h"
 
+Point::Point()
+{
+    x=0;
+    y=0;
+    z=0;
+    return;
+}
+
+Point::Point(double X, double Y, double Z)
+{
+    x=X;
+    y=Y;
+    z=Z;
+    return;
+}
+
+void Point::setX(double newX)
+{
+    x=newX;
+    return;
+}
+
+void Point::setY(double newY)
+{
+    y=newY;
+    return;
+}
+
+void Point::setZ(double newZ)
+{
+    z=newZ;
+    return;
+}
+
+double Point::getX() const
+{
+    return x;
+}
+
+double Point::getY() const
+{
+    return y;
+}
+
+double Point::getZ() const
+{
+    return z;
+}
+
+double Point::distanceTo(const Point &otherP) const
+{
+    double dis;
+    dis = sqrt( pow(otherP.x - x,2) + pow(otherP.y - y,2) + pow(otherP.z - z,2));
+    return dis;
+}
