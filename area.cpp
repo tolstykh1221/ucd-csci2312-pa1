07c074d45331047b8b2153811eb042dc180db9c5
tolstykh1221 <slava.tolstykh@hotmail.com>
2016-02-07 21:35:48 -0700

    Update area.cpp

--- area.cpp
+++ area.cpp
@@ -1,4 +1,15 @@
-//
-// Created by Slava on 2/7/2016.
-//
+#include <cmath>
+#include "Point.h"
 
+double computeArea(const Point &a, const Point &b, const Point &c)
+{
+    double sideA,sideB,sideC,semiP,area;
+    sideA = a.distanceTo(b);
+    sideB = b.distanceTo(c);
+    sideC = c.distanceTo(a);
+    semiP = (sideA + sideB + sideC)/2;
+
+    area = sqrt( semiP * (semiP - sideA )*(semiP - sideB)*(semiP - sideC) );
+
+    return area;
+}
